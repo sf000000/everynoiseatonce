0e0c95050e0805ed438b1a4f19057e9e3614455b
Jawad <bawskiya@gmail.com>
2024-01-28 16:21:30 -0600

    feat(latest-blogs.tsx): add blog fetching and display functionality

--- app/components/latest-blogs.tsx
+++ app/components/latest-blogs.tsx
@@ -1,14 +1,50 @@
+"use client";
+
+import { ComponentProps, useEffect, useState } from "react";
+
+import { BlogPost } from "@/interfaces";
 import { cn } from "@/lib/utils";
-import { ComponentProps } from "react";
+
+import axios from "axios";
+import Link from "next/link";
 
 export default function LatestBlogs({
   className,
   ...props
 }: ComponentProps<"div">) {
+  const [blogs, setBlogs] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get("/api/blogs");
+        setBlogs(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
   return (
     <div className={cn("mt-5", className)} {...props}>
       <h1 className="font-medium mb-4 text-lg">Latest Blogs</h1>
-      <p>WIP</p>
+      <div className="flex flex-col space-y-2">
+        {blogs.slice(0, 3).map((blog) => (
+          <div className="flex items-center justify-between" key={blog.slug}>
+            <Link
+              className="text-sm font-medium hover:underline"
+              href={`/blogs/${blog.slug}`}
+            >
+              {blog.meta.title}
+            </Link>
+            <span className="text-muted-foreground text-sm">
+              {blog.meta.date}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
