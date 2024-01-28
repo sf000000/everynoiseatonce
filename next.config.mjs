2620acefef1864097dd74905009034bfc2519671
Jawad <bawskiya@gmail.com>
2024-01-28 08:24:18 -0600

    update remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lastfm.freetls.fastly.net",
-      },
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
+        hostname: "**",
       },
     ],
   },
