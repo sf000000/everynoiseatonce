114d8d49487b7217198871e812ee7088bf965bf4
Jawad <bawskiya@gmail.com>
2024-01-28 08:25:02 -0600

    feat: add route for fetching blog content

---
+++ app/api/blogs/[slug]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export async function GET(req: NextRequest) {
+  const slug = new URL(req.url).pathname.split("/").pop();
+
+  try {
+    const markdownFile = fs.readFileSync(
+      path.join("blogs", slug + ".mdx"),
+      "utf-8"
+    );
+
+    const { data: meta, content } = matter(markdownFile);
+
+    return NextResponse.json({
+      meta,
+      content,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
+}
