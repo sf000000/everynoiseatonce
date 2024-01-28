96da8b41a2652dcdd2de153af2183f249133104f
Jawad <bawskiya@gmail.com>
2024-01-28 16:20:46 -0600

    style(animated-border-badge.tsx): change background class from bg-card/70 to bg-popover

--- app/components/animated-border-badge.tsx
+++ app/components/animated-border-badge.tsx
@@ -19,7 +19,7 @@ export default function AnimatedBorderBadge({
       {...props}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="select-none inline-flex h-full w-full items-center justify-center rounded-full bg-card/70 px-2 py-[1px] text-sm font-medium backdrop-blur-3xl">
+      <span className="select-none inline-flex h-full w-full items-center justify-center rounded-full bg-popover px-2 py-[1px] text-sm font-medium backdrop-blur-3xl">
         {label}
       </span>
     </div>
