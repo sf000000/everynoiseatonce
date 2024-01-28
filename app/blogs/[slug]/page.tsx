4984d60c203c4008894a5bb606cd2bec014560bb
Jawad <bawskiya@gmail.com>
2024-01-28 08:25:22 -0600

    feat: add page for blog posts

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import { ChevronLeftIcon } from "lucide-react";
+import Link from "next/link";
+
+import BlogPost from "@/app/components/blog-post";
+
+export default function Page({ params }: { params: { slug: string } }) {
+  return (
+    <main className="flex items-center justify-between w-full flex-col p-8 min-h-screen">
+      <div className="w-full max-w-3xl">
+        <Link className="flex items-center gap-x-2" href="/">
+          <ChevronLeftIcon className="w-4 h-4" />
+          <span>Home</span>
+        </Link>
+        <BlogPost slug={params.slug} />
+      </div>
+    </main>
+  );
+}
