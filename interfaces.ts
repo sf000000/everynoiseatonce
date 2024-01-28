a0e67e35dce9cc8637d75329d07ec1cd47d3f82e
Jawad <bawskiya@gmail.com>
2024-01-27 20:11:41 -0600

    add interfaces

---
+++ interfaces.ts
@@ -0,0 +1,58 @@
+export interface Profile {
+  name: string;
+  age: string;
+  subscriber: string;
+  realname: string;
+  bootstrap: string;
+  playcount: string;
+  artist_count: string;
+  playlists: string;
+  track_count: string;
+  album_count: string;
+  image: {
+    size: string;
+    url: string;
+  }[];
+  registered: {
+    unixtime: string;
+    text: number;
+  };
+  country: string;
+  gender: string;
+  url: string;
+  type: string;
+}
+
+export interface Track {
+  artist: Artist;
+  streamable: string;
+  image: Image[];
+  mbid: string;
+  album: Album;
+  name: string;
+  "@attr": Attr;
+  url: string;
+  date: {
+    uts: string;
+    "#text": string;
+  };
+}
+
+export interface Artist {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Image {
+  size: string;
+  "#text": string;
+}
+
+export interface Album {
+  mbid: string;
+  "#text": string;
+}
+
+export interface Attr {
+  nowplaying: string;
+}
