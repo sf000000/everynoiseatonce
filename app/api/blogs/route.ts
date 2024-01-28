672090c0acb391ee394770ea25f375b360e8be60
Jawad <bawskiya@gmail.com>
2024-01-28 08:24:54 -0600

    feat: add GET route for fetching blogs

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export async function GET(req: NextRequest) {
+  try {
+    const blogDir = "blogs";
+
+    const files = fs.readdirSync(path.join(blogDir));
+
+    const blogs = files.map((filename) => {
+      const fileContent = fs.readFileSync(
+        path.join(blogDir, filename),
+        "utf-8"
+      );
+      const { data: frontMatter } = matter(fileContent);
+
+      return {
+        meta: frontMatter,
+        slug: filename.replace(".mdx", ""),
+      };
+    });
+
+    return NextResponse.json(blogs);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message });
+  }
+}
