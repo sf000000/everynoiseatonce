e7d51a654861b469fd92454b7c9609768a839c3a
Jawad <bawskiya@gmail.com>
2024-01-28 20:59:08 -0600

    * style(experience.tsx): update experience details and color scheme

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -10,29 +10,29 @@ export default function Experience({
       <h1 className="font-medium mb-4 text-lg">Experience</h1>
       <ol className="relative border-s">
         <li className="mb-10 ms-4">
-          <div className="absolute w-3 h-3 bg-indigo-500 rounded-full mt-1.5 -start-1.5 border "></div>
+          <div className="absolute w-3 h-3 bg-green-500 rounded-full mt-1.5 -start-1.5 border "></div>
           <div className="flex flex-row items-center gap-2">
-            <div className="text-md font-medium">Front-End Engineer</div>
+            <div className="text-md font-medium">Front-End Developer</div>
             <div className="inline-block uppercase text-xs rounded-full px-1 py-0 border border-zinc-500 text-muted-foreground">
               present
             </div>
           </div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
-            Company Name
+            Freelance
           </div>
         </li>
         <li className="mb-10 ms-4">
-          <div className="absolute w-3 h-3 bg-zinc-600 rounded-full mt-1.5 -start-1.5 border "></div>
+          <div className="absolute w-3 h-3 bg-zinc-500 rounded-full mt-1.5 -start-1.5 border "></div>
           <div className="text-md font-medium">API Integration Engineer</div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
-            Company Name (2019-2020)
+            Autocode (2019-2020)
           </div>
         </li>
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-zinc-400 rounded-full mt-1.5 -left-1.5 border "></div>
           <div className="text-md font-medium">Python Developer</div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
-            Company Name (2017-2019)
+            Pinecone (2017-2019)
           </div>
         </li>
       </ol>
