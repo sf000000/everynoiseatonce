ea57c7cee85020698661e8b8c35ab633ecb775af
Jawad <bawskiya@gmail.com>
2024-01-29 05:37:42 -0600

    * style(infinite-slider.tsx): adjust linear gradient color in InfiniteSlider component

--- app/components/mdx/infinite-slider.tsx
+++ app/components/mdx/infinite-slider.tsx
@@ -27,7 +27,7 @@ const LOGOS = [
 export const InfiniteSlider = () => {
   return (
     <div className="not-prose relative flex items-center justify-center rounded-xl border px-4 py-12 bg-secondary/5">
-      <div className="relative m-auto w-[500px] overflow-hidden bg-secondary/5 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#0c0c0f_0%,rgba(11,11,12,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#0c0c0f_0%,rgba(11,11,12,0)_100%)] after:content-['']">
+      <div className="relative m-auto w-[500px] overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(250px*10)]">
           {LOGOS.map((logo, index) => (
             <div
