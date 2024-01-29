e7be2f89195e2a46339ea0a1a04dd8b577f77c6e
Jawad <bawskiya@gmail.com>
2024-01-28 21:00:10 -0600

    * feat: add InfiniteSlider component to mdx components * feat: create new blog post 'create-infinite-slider-with-tailwind-css.mdx' * feat: add new highlight-js theme 'ir-black.css' for code syntax highlighting

---
+++ app/components/mdx/infinite-slider.tsx
@@ -0,0 +1,52 @@
+import {
+  FigmaLogoIcon,
+  FramerLogoIcon,
+  SketchLogoIcon,
+  TwitterLogoIcon,
+  GitHubLogoIcon,
+  VercelLogoIcon,
+  NotionLogoIcon,
+  DiscordLogoIcon,
+  InstagramLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+
+const LOGOS = [
+  <FigmaLogoIcon className="text-white w-8 h-8" />,
+  <FramerLogoIcon className="text-white w-8 h-8" />,
+  <SketchLogoIcon className=" text-white w-8 h-8" />,
+  <TwitterLogoIcon className="text-white w-8 h-8" />,
+  <GitHubLogoIcon className="text-white w-8 h-8" />,
+  <VercelLogoIcon className="text-white w-8 h-8" />,
+  <NotionLogoIcon className="text-white w-8 h-8" />,
+  <DiscordLogoIcon className="text-white w-8 h-8" />,
+  <InstagramLogoIcon className="text-white w-8 h-8" />,
+  <LinkedInLogoIcon className="text-white w-8 h-8" />,
+];
+
+export const InfiniteSlider = () => {
+  return (
+    <div className="not-prose relative flex items-center justify-center rounded-xl border px-4 py-12 bg-secondary/5">
+      <div className="relative m-auto w-[500px] overflow-hidden bg-secondary/5 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] after:content-['']">
+        <div className="animate-infinite-slider flex w-[calc(250px*10)]">
+          {LOGOS.map((logo, index) => (
+            <div
+              className="slide flex w-[125px] items-center justify-center"
+              key={index}
+            >
+              {logo}
+            </div>
+          ))}
+          {LOGOS.map((logo, index) => (
+            <div
+              className="slide flex w-[125px] items-center justify-center"
+              key={index}
+            >
+              {logo}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
