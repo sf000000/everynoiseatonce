0cd6ed98c256a15e4cc516b06d09472bc3450929
Jawad <bawskiya@gmail.com>
2024-01-28 20:58:46 -0600

    * refactor(blogs-list.tsx): simplify animation logic and remove unused imports * feat(blogs-list.tsx): add Link component to blog list items for navigation

--- app/components/blogs-list.tsx
+++ app/components/blogs-list.tsx
@@ -1,33 +1,19 @@
-"use client";
-
-import { cn } from "@/lib/utils";
-import { ComponentProps, useState, useEffect } from "react";
-
+import { ComponentProps, useEffect, useState } from "react";
 import axios from "axios";
+import { motion, useAnimation } from "framer-motion";
+import Link from "next/link";
 import { BlogPost } from "@/interfaces";
-import BlogListItem from "./blog-list-item";
-
-import { motion } from "framer-motion";
-
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
 
 const itemVariants = {
   hidden: { y: 20, opacity: 0 },
-  visible: {
+  visible: (i: number) => ({
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.5,
+      delay: i * 0.1,
+      duration: 0.3,
     },
-  },
+  }),
 };
 
 export default function BlogsList({
@@ -35,32 +21,40 @@ export default function BlogsList({
   ...props
 }: ComponentProps<typeof motion.div>) {
   const [blogs, setBlogs] = useState<BlogPost[]>([]);
+  const controls = useAnimation();
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const response = await axios.get("/api/blogs");
         setBlogs(response.data);
+        controls.start((i) => "visible");
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchBlogs();
-  }, []);
+  }, [controls]);
 
   return (
-    <motion.div
-      {...props}
-      className={cn("", className)}
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-    >
+    <motion.div {...props} className={className} initial="hidden">
       <div className="flex flex-col space-y-2">
-        {blogs.map((blog) => (
-          <motion.div key={blog.slug} variants={itemVariants}>
-            <BlogListItem className="" blog={blog} />
+        {blogs.map((blog, index) => (
+          <motion.div
+            key={blog.slug}
+            custom={index} // Pass index to use in the variant
+            variants={itemVariants}
+            initial="hidden"
+            animate={controls}
+          >
+            <Link
+              href={`/blogs/${blog.slug}`}
+              className="flex items-center justify-between text-muted-foreground hover:text-primary transition-colors duration-200"
+            >
+              <h4 className="text-sm font-medium">{blog.meta.title}</h4>
+              <span className="text-sm">{blog.meta.date}</span>
+            </Link>
           </motion.div>
         ))}
       </div>
