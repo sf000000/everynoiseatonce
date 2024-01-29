f6991ad7232b1b85a75e87b2d17c8c5d7571701f
Jawad <bawskiya@gmail.com>
2024-01-28 20:59:39 -0600

    * feat(info-section.tsx): add drag and drop easter egg to avatar image * feat(open-to-work.tsx): create new OpenToWork component * refactor(info-section.tsx): replace AnimatedBorderBadge with OpenToWork component

--- app/components/info-section.tsx
+++ app/components/info-section.tsx
@@ -1,30 +1,66 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { ComponentProps } from "react";
+import { ComponentProps, useState } from "react";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
-import { ModeToggle } from "@/app/components/mode-toggle";
-import AnimatedBorderBadge from "@/app/components/animated-border-badge";
+import OpenToWork from "@/app/components/open-to-work";
+
+import { motion } from "framer-motion";
 
 export default function InfoSection({
   className,
   ...props
 }: ComponentProps<"div">) {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragEnd = () => {
+    setTimeout(() => {
+      setIsDragging(false);
+    }, 200);
+  };
+
   return (
     <div className={cn("", className)} {...props}>
-      <Link className="flex items-center justify-between" href="/">
-        <Image
-          src="/images/avatar.jpg"
-          alt="avatar"
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
-        <ModeToggle />
-      </Link>
+      <div className="relative">
+        {/* Placeholder */}
+        <div
+          className={cn(
+            "absolute w-[50px] h-[50px] rounded-full",
+            isDragging ? "border-2 border-dashed" : "hidden"
+          )}
+          style={{ top: 0, left: 0 }}
+        ></div>
+
+        {/* Draggable Image */}
+        <motion.div
+          drag
+          dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+          dragElastic={0.4}
+          dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
+          dragSnapToOrigin={true}
+          className="select-none w-fit"
+          onDragStart={() => setIsDragging(true)}
+          onDragEnd={handleDragEnd}
+        >
+          <Link draggable={false} href="/">
+            <div>
+              <Image
+                src="/images/avatar.jpg"
+                alt="avatar"
+                width={50}
+                height={50}
+                className="rounded-full"
+                draggable={false}
+              />
+            </div>
+          </Link>
+        </motion.div>
+      </div>
       <div className="flex items-center gap-x-2 mt-2">
         <h1 className="font-medium text-xl">Jawad</h1>
-        <AnimatedBorderBadge label="Open to work" />
+        <OpenToWork />
       </div>
       <p className="text-muted-foreground">Web Developer</p>
       <div className="flex flex-row justify-between items-center mt-6">
---
+++ app/components/open-to-work.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+
+export default function OpenToWork({
+  className,
+  ...props
+}: ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "select-none group relative grid overflow-hidden rounded-full px-2 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200",
+        className
+      )}
+      {...props}
+    >
+      <span>
+        <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+      </span>
+      <span className="backdrop absolute inset-[1px] rounded-full bg-neutral-950 transition-colors duration-200" />
+      <span className="z-10 py-0.5 text-xs">Open to work</span>
+    </div>
+  );
+}
