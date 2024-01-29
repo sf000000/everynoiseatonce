c30af3a56e3dabfa670a359dc7e9e86534ec6035
Jawad <bawskiya@gmail.com>
2024-01-29 05:37:52 -0600

    * feat(pre.tsx): update children prop type and add additional styling to pre component

--- app/components/mdx/pre.tsx
+++ app/components/mdx/pre.tsx
@@ -8,13 +8,17 @@ export function Pre({
   buttonClasses = "absolute top-3 right-3 bg-zinc-900",
   ...props
 }: {
-  children: any;
+  children: React.ReactNode;
   raw: string;
   buttonClasses?: string;
 }) {
   return (
-    <pre {...props} className={clsx("relative")}>
+    <pre {...props} className={clsx("relative text-[15px]")}>
+      <div className="absolute inset-0 -z-10 h-full w-full border rounded-md bg-secondary/5 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-full w-full rounded-full bg-indigo-500/5 blur-[100px]"></div>
+      </div>
       {children}
+
       <CopyButton text={raw} className="absolute p-2 m-2 top-3 right-3" />
     </pre>
   );
