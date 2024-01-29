fc57f9a1fb95d226c90e81b5817a7a6302e1ef9a
Jawad <bawskiya@gmail.com>
2024-01-28 21:29:22 -0600

    * feat(layout.tsx): add Vercel Analytics component to layout * chore(package.json, package-lock.json): add @vercel/analytics package to dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
