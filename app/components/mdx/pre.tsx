f7e85ff0fc1e0dcc143ec0f2d7bd1aa5d976120c
Jawad <bawskiya@gmail.com>
2024-01-29 17:58:20 -0600

    refactor(pre.tsx): add JetBrains Mono font and apply it to pre element

--- app/components/mdx/pre.tsx
+++ app/components/mdx/pre.tsx
@@ -1,6 +1,14 @@
 import clsx from "clsx";
+import { JetBrains_Mono } from "next/font/google";
 
 import { CopyButton } from "@/components/copy-button";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["200", "400", "600"],
+  preload: true,
+});
 
 export function Pre({
   children,
@@ -13,12 +21,14 @@ export function Pre({
   buttonClasses?: string;
 }) {
   return (
-    <pre {...props} className={clsx("relative text-[15px]")}>
+    <pre
+      {...props}
+      className={clsx("relative text-[15px]", jetbrainsMono.className)}
+    >
       <div className="absolute inset-0 -z-10 h-full w-full bg-secondary/10 border rounded-lg bg-[radial-gradient(rgba(48,45,42,0.2)_1px,transparent_1px)] [background-size:16px_16px]">
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-full w-full rounded-full bg-indigo-500/5 blur-[100px]"></div>
       </div>
       {children}
-
       <CopyButton text={raw} className="absolute p-2 m-2 top-3 right-3" />
     </pre>
   );
