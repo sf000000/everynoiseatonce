2099ed43797e481f7214c424ff4cd1c1225494c4
Jawad <bawskiya@gmail.com>
2024-01-29 01:42:59 -0600

    * style(infinite-slider.tsx): increase logo icon size and adjust gradient color

--- app/components/mdx/infinite-slider.tsx
+++ app/components/mdx/infinite-slider.tsx
@@ -12,22 +12,22 @@ import {
 } from "@radix-ui/react-icons";
 
 const LOGOS = [
-  <FigmaLogoIcon className="text-white w-8 h-8" key="figma" />,
-  <FramerLogoIcon className="text-white w-8 h-8" key="framer" />,
-  <SketchLogoIcon className=" text-white w-8 h-8" key="sketch" />,
-  <TwitterLogoIcon className="text-white w-8 h-8" key="twitter" />,
-  <GitHubLogoIcon className="text-white w-8 h-8" key="github" />,
-  <VercelLogoIcon className="text-white w-8 h-8" key="vercel" />,
-  <NotionLogoIcon className="text-white w-8 h-8" key="notion" />,
-  <DiscordLogoIcon className="text-white w-8 h-8" key="discord" />,
-  <InstagramLogoIcon className="text-white w-8 h-8" key="instagram" />,
-  <LinkedInLogoIcon className="text-white w-8 h-8" key="linkedin" />,
+  <FigmaLogoIcon className="text-white w-12 h-12" key="figma" />,
+  <FramerLogoIcon className="text-white w-12 h-12" key="framer" />,
+  <SketchLogoIcon className=" text-white w-12 h-12" key="sketch" />,
+  <TwitterLogoIcon className="text-white w-12 h-12" key="twitter" />,
+  <GitHubLogoIcon className="text-white w-12 h-12" key="github" />,
+  <VercelLogoIcon className="text-white w-12 h-12" key="vercel" />,
+  <NotionLogoIcon className="text-white w-12 h-12" key="notion" />,
+  <DiscordLogoIcon className="text-white w-12 h-12" key="discord" />,
+  <InstagramLogoIcon className="text-white w-12 h-12" key="instagram" />,
+  <LinkedInLogoIcon className="text-white w-12 h-12" key="linkedin" />,
 ];
 
 export const InfiniteSlider = () => {
   return (
     <div className="not-prose relative flex items-center justify-center rounded-xl border px-4 py-12 bg-secondary/5">
-      <div className="relative m-auto w-[500px] overflow-hidden bg-secondary/5 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#0b0b0c_0%,rgba(11,11,12,0)_100%)] after:content-['']">
+      <div className="relative m-auto w-[500px] overflow-hidden bg-secondary/5 before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,#0c0c0f_0%,rgba(11,11,12,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,#0c0c0f_0%,rgba(11,11,12,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(250px*10)]">
           {LOGOS.map((logo, index) => (
             <div
