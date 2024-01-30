28ba7f482064f7e857b186994dfa43ddc212148b
Jawad <bawskiya@gmail.com>
2024-01-29 21:21:27 -0600

    refactor(blog-post.tsx): make blog title responsive

--- app/components/blog-post.tsx
+++ app/components/blog-post.tsx
@@ -84,9 +84,11 @@ export default function BlogPost({ slug, className, ...props }: BlogPostProps) {
         exit={{ opacity: 0 }}
         className="prose prose-neutral mx-auto min-w-full pb-12 dark:prose-dark prose-h1:text-xl prose-h1:font-medium prose-h2:text-lg prose-h2:font-medium prose-h3:text-base prose-h3:font-medium prose-a:font-normal prose-figcaption:text-center prose-strong:font-semibold prose-img:mb-0 prose-video:mb-0 !prose-invert prose-code:bg-indigo-500/10 prose-code:rounded prose-code:px-0.5 prose-code:py-0.5 prose-code:text-indigo-500 prose-a:no-underline prose-a:text-indigo-500"
       >
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-semibold m-0">{blog?.meta.title}</h1>
-          <span className="text-sm text-muted-foreground self-center">
+        <div className="flex flex-col sm:flex-row items-center justify-between">
+          <h1 className="text-2xl text-center sm:text-start font-semibold m-0">
+            {blog?.meta.title}
+          </h1>
+          <span className="text-sm mt-2 sm:mt-0 text-muted-foreground self-center">
             {blog?.meta.date}
           </span>
         </div>
