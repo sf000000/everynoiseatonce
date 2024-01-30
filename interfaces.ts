0d726ff47f663178ed7232268a429f79317acac5
Jawad <bawskiya@gmail.com>
2024-01-30 00:34:19 -0600

    refactor: add TopArtist interface to interfaces.ts

--- interfaces.ts
+++ interfaces.ts
@@ -63,3 +63,22 @@ export interface BlogPost {
   };
   content: string;
 }
+
+export interface TopArtist {
+  streamable: string;
+  image: {
+    size: string;
+    "#text": string;
+  };
+
+  mbid: string;
+  url: string;
+  playcount: string;
+  "@attr": {
+    rank: string;
+  };
+
+  name: string;
+  spotifyId: string;
+  artwork: string;
+}
