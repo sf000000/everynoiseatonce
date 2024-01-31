2c7920f15ca8a85c63aadb36f64cec8dcc4235e8
Jawad <bawskiya@gmail.com>
2024-01-31 01:02:09 -0600

    refactor(experience.tsx): change grammar, and use badge component

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,5 +1,7 @@
-import { cn } from "@/lib/utils";
 import { ComponentProps } from "react";
+
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 export default function Experience({
   className,
@@ -12,24 +14,22 @@ export default function Experience({
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-green-500 rounded-full mt-1.5 -start-1.5 border "></div>
           <div className="flex flex-row items-center gap-2">
-            <div className="text-md font-medium">Front-End Developer</div>
-            <div className="inline-block uppercase text-xs rounded-full px-1 py-0 border border-zinc-500 text-muted-foreground">
-              present
-            </div>
+            <div className="text-md font-medium">Frontend Developer</div>
+            <Badge variant="outline">Current</Badge>
           </div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
             Freelance
           </div>
         </li>
         <li className="mb-10 ms-4">
-          <div className="absolute w-3 h-3 bg-zinc-500 rounded-full mt-1.5 -start-1.5 border "></div>
+          <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border "></div>
           <div className="text-md font-medium">API Integration Engineer</div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
             Autocode (2019-2020)
           </div>
         </li>
         <li className="mb-10 ml-4">
-          <div className="absolute w-3 h-3 bg-zinc-400 rounded-full mt-1.5 -left-1.5 border "></div>
+          <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -left-1.5 border "></div>
           <div className="text-md font-medium">Python Developer</div>
           <div className="mb-4 text-sm font-normal text-muted-foreground">
             Pinecone (2017-2019)
